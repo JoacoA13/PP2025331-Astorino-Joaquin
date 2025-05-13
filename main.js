22ae682e6598822d39145425858a90ca01cd3f86
JoacoA13 <joaquin.astorino@gmail.com>
2025-05-12 21:56:29 -0300

    primer commit

---
+++ main.js
@@ -0,0 +1,121 @@
+import {Serie} from "./serie.js";
+
+function $(x) {
+    return document.getElementById(x)
+}
+const divSeries = $("series");
+const btnAnterior = $("btnAnterior");
+const btnSiguiente = $("btnSiguiente");
+const guardados = $("guardados");
+
+const url = "https://api.tvmaze.com/shows/"
+const optionsGET = {
+    method: "GET",
+    headers: {}
+};
+
+const cantidadSeries = 6;
+const idsUsados = [];
+const paginas = new Array();
+
+async function traerSerie(id) {
+    try {
+        const response = await fetch(url+id,optionsGET)
+        if (response.status === 200) {
+            const serieJSON = await response.json();
+            serieJSON.image = serieJSON.image.medium;
+            const serie = Serie.createFromJsonString(JSON.stringify(serieJSON));
+            //const serie = new Serie(serieJSON.id,serieJSON.url,serieJSON.name,serieJSON.language,serieJSON.genres,serieJSON.image.medium);
+            return serie;
+        }else {
+            console.error("Error HTTP:", response.status);
+            return [];
+        }
+    } catch (error) {
+        window.alert("Error al cargar los datos" + error)
+        return [];
+    }
+}
+
+async function crearSeries(cantidad) {
+    const nuevasSeries = [];
+    let intentos = 0;
+
+    for (let i = 0; i < cantidad && intentos < 500;) {
+        let id = Math.floor(Math.random() * 12) + 1;
+        intentos++;
+
+        if (!idsUsados.includes(id)) {
+            const serie = await traerSerie(id);
+            if (serie) {
+                nuevasSeries.push(serie);
+                idsUsados.push(id);
+                i++;
+            }
+        }
+    }
+
+    return nuevasSeries;
+}
+
+
+function renderizarSeries(lista) {
+    divSeries.innerHTML = "";
+    lista.forEach(serie => {
+        divSeries.appendChild(serie.createHtmlElement());
+    });
+}
+
+const pag1 = await crearSeries(cantidadSeries);
+paginas.push(pag1);
+let paginaActual = 0;
+renderizarSeries(pag1);
+
+
+async function paginaSiguiente() {
+    paginaActual++;
+
+    if (paginaActual < paginas.length) {
+        renderizarSeries(paginas[paginaActual]);
+    }else {
+        const nuevasSeries = await crearSeries(cantidadSeries);
+        if (nuevasSeries.length === 0) {
+            paginaActual--;
+            alert("No hay más series nuevas para mostrar");
+        } else {
+            paginas.push(nuevasSeries);
+            renderizarSeries(nuevasSeries);
+        }
+    }
+}
+
+async function paginaAnterior() {
+    if (paginaActual > 0) {
+        paginaActual--;
+        renderizarSeries(paginas[paginaActual]);
+    } else {
+        alert("Ya estás en la primera página");
+    }
+}
+
+btnSiguiente.addEventListener("click",async(e)=>{
+    e.preventDefault();
+    paginaSiguiente();
+});
+
+btnAnterior.addEventListener("click",async(e)=>{
+    e.preventDefault();
+    paginaAnterior();
+});
+
+guardados.addEventListener("click", async(e) =>{
+    e.preventDefault();
+    const p = $("listaSeriesGuardadas");
+
+    const guardadas = JSON.parse(localStorage.getItem("seriesGuardadas")) || [];
+
+    console.log(guardadas);
+    p.innerText(guardadas.stringify());
+    
+});
+
---
+++ serie.js
@@ -0,0 +1,79 @@
+export class Serie {
+    id;
+    url;
+    name;
+    language;
+    genres;
+    image;
+    constructor(id,url,name,language,generes,image) {
+        this.id = id;
+        this.url = url;
+        this.name = name;
+        this.language = language;
+        this.generes = generes;
+        this.image = image;
+    }
+
+    toJsonString(){
+        return JSON.stringify({id: this.id,url: this.url,name: this.name,language: this.language,genres: this.genres,image: this.image})
+    }
+
+    static createFromJsonString(json) {
+        const data = JSON.parse(json);
+        return new Serie(data.id,data.url,data.name,data.language,data.genres,data.image);
+    }
+
+    createHtmlElement() {
+        const div = document.createElement('div');
+        const h2 = document.createElement('h2');
+        h2.textContent = this.name;
+        const pLang = document.createElement('p');
+        pLang.textContent = `Lenguaje: ${this.language}`;
+        const pGenres = document.createElement('p');
+
+        pGenres.textContent = `Géneros: ${this.generes.join(', ')}`;
+        const a = document.createElement('a');
+        a.href = this.url;
+        a.target = "_blank";
+
+        const img = document.createElement('img');
+        img.src = this.image;
+        img.alt = this.name;
+        img.width = 200;
+        a.appendChild(img);
+
+        const br = document.createElement('br');
+
+        const btnGuardar = document.createElement('button');
+        btnGuardar.textContent = 'Guardar';
+        btnGuardar.addEventListener("click", async (e) => {
+            console.log("Click en:", this.name);
+            this.guardarSerie(this)
+        });
+
+        div.appendChild(h2);
+        div.appendChild(pLang);
+        div.appendChild(pGenres);
+        div.appendChild(a);
+        div.appendChild(br);
+        div.appendChild(btnGuardar);
+
+        return div;
+    }
+
+    guardarSerie(serie) {
+        const guardadas = JSON.parse(localStorage.getItem("seriesGuardadas")) || [];
+        const jsonSerie = serie.toJsonString();
+
+        const existente = guardadas.find(seriesGuardada => seriesGuardada.id === serie.id);
+        if (existente) {
+            alert(`La serie "${serie.name}" ya está guardada!`);
+            return;
+        }
+
+        guardadas.push(JSON.parse(jsonSerie));
+    
+        localStorage.setItem("seriesGuardadas", JSON.stringify(guardadas));
+        console.log(`¡Serie "${serie.name}" guardada!`);
+    }
+}
